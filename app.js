3bbe3394f00ff41e7f3462a01d800f6e1b4c07a9
Kirti Jha <kirtijha@in.ibm.com>
2019-01-04 18:44:30 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+var exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useNewUrlParser: true
+})
+.then(() => console.log('MongoDB Connected....'))
+.catch(err => console.log(err));
+
+// Load Idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title
+  });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas
+      });
+    })
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title) {
+    errors.push({
+      text: 'Please add a title'
+    });
+  }
+  if(!req.body.details) {
+    errors.push({
+      text: 'Please add some details'
+    });
+  }
+
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      })
+  }
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
